994cc73f815688c24330e881449b8808ed5db96a
ippsec <root@ippsec.rocks>
2024-01-15 22:26:52 -0500

    Code cleanup

--- pwnclosure/pwnclosure.py
+++ pwnclosure/pwnclosure.py
@@ -32,30 +32,22 @@ def saveFile(fullPath, content):
 # ToDo: Automatically identify parent based upon webserver config
 # func = a call to download src
 def startCrawl(func, parent, file):    
-    crawled = []
-    queue = [file]
-    while len(queue) > 0:
-        for page in queue:    
-            #logging.info(f"Downloading: {page}")            
-            queue.remove(page)        
-            crawled.append(page)
-            fullPath = parent + page
-            output = func(fullPath)
-            saveFile(fullPath, output)
-
-            directory = ''
-            if '/' in page:
-                directory = os.path.dirname(page)
-            links = getLinks(output)
-            for link in links:
-                link = link.lstrip("/")                
-                if link.endswith(".."):
-                    continue
-                if directory:
-                    link = directory + "/" + link
-                if (link not in crawled) and (link not in queue):
-                    queue.append(link)
-                    logging.debug(f"Added {link} to queue")
+    crawled = set()
+    queue = {file}
+    while queue:
+        page = queue.pop()
+        crawled.add(page)
+        fullPath = parent + page
+        output = func(fullPath)
+        saveFile(fullPath, output)
+        links = getLinks(output)
+        for link in links:
+            link = link.lstrip("/")
+            if link.endswith(".."):
+                continue
+            if (link not in crawled) and (link not in queue):
+                queue.add(link)
+                logging.debug(f"Added {link} to queue")       
 
 startCrawl(fd.downloadFile, "/var/www/html/", "index.php")
 
